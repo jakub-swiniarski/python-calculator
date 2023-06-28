9640ccba2d5bd12bb752afa6f5009519611c3b8e
Jakub Świniarski <jakub.swiniarski2007@gmail.com>
2023-06-28 18:50:51 +0200

    render buttons

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ window.configure(bg="#303030")
 window.resizable(False, False)
 
 #calculator display
-display=tk.Label(window, text="example text", width=280, fg="#ffffff",bg="#474747", anchor="w", padx=10, pady=15)
-display.config(font=("Courier New",20))
+display=tk.Label(window, text="", width=280, fg="#ffffff",bg="#474747", anchor="w", padx=15, pady=15)
+display.config(font=("Courier New",24))
 display.pack(padx=10, pady=10)
 
 #buttons
-labels=[["0", "1", "4", "7", "AC"],
+labels=[["0", "1", "4", "7", "C"],
         [".", "2", "5", "8", "("],
         ["%", "3", "6", "9", ")"],
         ["=", "+", "-","×", "÷"]]
-buttons=[[0 for x in range(4)]for y in range(5)]
-#for i in range(4):
-#    for j in range(5):
-#        buttons[i][j] = tk.Button(window)
-print(labels[3][3])
-
+buttons=[[0 for x in range(5)]for y in range(4)]
+for i in range(4):
+    for j in range(5):
+        buttons[i][j] = tk.Button(window, text=labels[i][j], fg="#ffffff",bg="#474747", anchor="center")
+        buttons[i][j].config(font=("Courier New",24))
+        buttons[i][j].place(x=10+i*78, y=400-(j+1)*60)
+        
 window.mainloop()
