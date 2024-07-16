16a8bc75420423760535db8bf45b0885dc0bc243
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-16 10:20:40 +0200

    improve code readability

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ window.title ="Calculator"
 window.geometry("300x400")
 window.configure(bg = "#303030")
 window.resizable(False, False)
-input = ""
-display = tk.Label(window, text = input, width = 280, fg = "#ffffff", bg = "#474747", anchor = "w", padx = 15, pady = 15)
+input   = ""
+display = tk.Label(
+    window,
+    text   = input,
+    width  = 280,
+    fg     = "#ffffff",
+    bg     = "#474747",
+    anchor = "w",
+    padx   = 15,
+    pady   = 15
+)
 display.config(font = ("Courier New", 24))
 display.pack(padx = 10, pady = 10)
 
@@ -38,8 +47,15 @@ def main():
     buttons = [[0 for x in range(5)] for y in range(4)]
     for i in range(4):
         for j in range(5):
-            buttons[i][j] = tk.Button(window, text = labels[i][j], fg = "#ffffff", bg = "#474747", anchor = "center", command = lambda i = i, j = j: clicked(labels[i][j]))
-            buttons[i][j].config(font = ("Courier New", 24))
+            buttons[i][j] = tk.Button(
+                window,
+                text    = labels[i][j],
+                font    = ("Courier New", 24),
+                fg      = "#ffffff",
+                bg      = "#474747",
+                anchor  = "center",
+                command = lambda i = i, j = j: clicked(labels[i][j])
+            )
             buttons[i][j].place(x = 10 + i * 78, y = 400 - (j + 1) * 60)
 
     buttons[0][4].config(command=clear)
